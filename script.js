6172d75b6e2aa6da5bc7814657d1813e273a53a0
EvyBettina <evybettina@hotmail.com>
2024-09-25 11:55:32 +0200

    setup js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Hello!');
+let currentImage = 0
+
+//Elementene vi trenger fra HTML
+let allImages = document.querySelectorAll(".carusell img");
+
+function showImage(imageindex) {}
+
+function moveImage(offset) {
+// Hvilket bilde er vi på nå
+
+// Regn ut hvilket bilde som blir neste
+let newImageIndex = currentImage + offset;
+
+for (let image of allImages) {
+    console.log(image);
+// Fjern "current-image" klassen fra nåværende bilde
+//Legg til "current-image" på det nye bilde
+}
+}
+console.log(allImages);
+moveImage(1);
+console.log(allImages);
